8618d5a9fc8fa825789201b2937788099f2b8c6d
Bagas <madhawybagas11@gmail.com>
2020-02-09 07:13:55 +0700

    halo

---
+++ app/src/main/java/com/example/bungagila/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bungagila;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.teks1);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+    }
+}
